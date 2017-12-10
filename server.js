2288650ac076744a3df62cf35c35d4ef6685eeb7
stanley <chw058@gmail.com>
2017-12-10 12:01:28 -0800

    deployment test

--- server.js
+++ server.js
@@ -70,10 +70,17 @@ app.use("/", savedItemRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require('path');
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     console.log(keys);
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
+} else {
+
+  app.get("*", function(req, res) {
+    console.log(keys);
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+
 }
 
 /*
