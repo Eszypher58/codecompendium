f0c7ed232d4e7bafb3b803338e639069b0ce34a3
stanley <chw058@gmail.com>
2017-12-10 16:17:31 -0800

    non-passport Google Oauth

--- client/build/googleauthlogin.js
+++ client/build/googleauthlogin.js
@@ -7,21 +7,25 @@ function onSignIn(googleUser) {
     } else {
       //go to sign up page
     }*/
-    console.log("ID: " + googleprofile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + googleprofile.getName());
-    console.log('Given Name: ' + googleprofile.getGivenName());
-    console.log('Family Name: ' + googleprofile.getFamilyName());
-    console.log("Image URL: " + googleprofile.getImageUrl());
-    console.log("Email: " + googleprofile.getEmail());
+    //console.log("ID: " + googleprofile.getId()); // Don't send this directly to your server!
+    //console.log('Full Name: ' + googleprofile.getName());
+    //console.log('Given Name: ' + googleprofile.getGivenName());
+    //console.log('Family Name: ' + googleprofile.getFamilyName());
+    //console.log("Image URL: " + googleprofile.getImageUrl());
+    //console.log("Email: " + googleprofile.getEmail());
   
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+    //console.log("ID Token: " + id_token);
     
     var myUserEntity = {};
     myUserEntity.Id = googleprofile.getId();
     myUserEntity.Name = googleprofile.getName();
+    myUserEntity.Email = googleprofile.getEmail();
+    myUserEntity.Photo = googleprofile.getImageUrl();
     sessionStorage.setItem('myUserEntity',JSON.stringify(myUserEntity));
+
+    //$.post("/api/check/" + googleprofile.getId())
   
     $.get("/", function() {
       window.location.href = "/" + googleprofile.getId() + "/mycollection";
