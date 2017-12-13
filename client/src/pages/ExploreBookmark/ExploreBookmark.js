07bf34451563d7973d18a0db6b2546e45856c1e3
Your NameChloe <chloechoudesign@gmail.com>
2017-12-12 21:12:36 -0800

    fix notification

--- client/src/pages/ExploreBookmark/ExploreBookmark.js
+++ client/src/pages/ExploreBookmark/ExploreBookmark.js
@@ -260,7 +260,7 @@ handleSubmitButton = (e) => {
             <br />
           </DeleteBookmark>*/}
 
-          <div class="notify"><span id="notifyType" class=""></span></div>
+         <div class="notify"><span id="notifyType" class=""></span></div>
 
 
       </div>
