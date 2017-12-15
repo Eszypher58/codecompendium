845117c9d26fbe8e955124239541e43b2227d209
Your NameChloe <chloechoudesign@gmail.com>
2017-12-14 19:20:33 -0800

    login btn styling

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -17,7 +17,7 @@ class Login extends Component {
                 </div>
 
                 <div class="card-body text-center">
-                  <a class="github-login" href="/auth/google">Log in with Google</a>
+                  {/*<a class="github-login" href="/auth/google">Log in with Google</a>*/}
 
                   <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
                   
